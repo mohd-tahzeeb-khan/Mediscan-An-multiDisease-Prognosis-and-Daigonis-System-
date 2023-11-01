008bd65a20d351557f622ca4cc3ac2328b5412e5
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2023-11-02 00:23:00 +0530

    working on app.py

--- app.py
+++ app.py
@@ -2,3 +2,28 @@ from flask import Flask, render_template, request, flash
 import flask
 import numpy as np
 import os
+from tensorflow.keras.utils import load_img
+from tensorflow.keras.utils import img_to_array
+from keras.models import load_model
+
+#loading models
+heart_model=load_model("")
+daibeties_model=load_model("")
+lungs_model=load_model("")
+skin_model=load_model("")
+brain_model=load_model("")
+
+class predict:
+    def __init__():
+
+app=Flask(__name__)
+app.secret_key="secret key"
+@app.route("/", methods=['GET', 'POST'])
+def home():
+    return render_template("index.html")
+@app.route("/credits", methods=["GET"])
+def credits():
+    return render_template("credits.html")
+@app.route("/help", methods=["GET"])
+def help():
+    return render_template("help.html")
