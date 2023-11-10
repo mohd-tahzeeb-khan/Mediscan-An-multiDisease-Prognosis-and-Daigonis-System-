e11ca7c2f4f88cae586a3746a06e1133215cc3f2
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2023-11-11 01:10:57 +0530

    working on application and enhancement of application

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ uploadedimges='static/userUpload'
 #daibeties_model=load_model("models\diabetes_model.pkl")
 lungs_model=load_model("models\Lungs_disease.h5")
 skin_model=load_model("models\Skin_disease4.h5")
-brain_model=load_model("models\brain_disease.h5")
+brain_model=load_model("models/brain_disease.h5")
 print("models loaded")
 def brain_models_prediction(brain_Images):
     test_image=load_img(brain_Images, target_size=(224, 224))
@@ -30,7 +30,7 @@ def brain_models_prediction(brain_Images):
         return "NO Tumor", 'index.html'
 #-----------------------------------------------------------------------------------------------------------------    
 def lungs_models_prediction(lungs_Images):
-    test_image=load_img(kungs_Images, target_size=(224, 224))
+    test_image=load_img(lungs_Images, target_size=(224, 224))
     test_image = img_to_array(test_image)/255 # convert image to np array and normalize
     test_image = np.expand_dims(test_image, axis = 0) # change dimention 3D to 4D
    
@@ -57,11 +57,11 @@ def skin_models_prediction(skin_Images):
     if pred == 0:
         return 'Acne', 'diganosis.html' # if index 0 burned leaf
     elif pred==1:
-        return 'Normal', 'index.html'
+        return 'Normal', index
     elif pred==2:
-        return "Vascular Tumors", 'index.html'
+        return "Vascular Tumors", index
     elif pred==3:
-        return "Fungal", 'index.html'
+        return "Fungal",index
 #------------------------------------------------------------------------------------------------------------
 app=Flask(__name__)
 app.secret_key="secret key"
@@ -80,19 +80,50 @@ def credits():
 @app.route("/about", methods=["GET"])
 def help():
     return render_template("upload.html")
+#-----------------------------------------------------------------------------------------------------------
 @app.route("/predict-brain", methods=['POST'])
 def predict_Brain():
     if request.method=='POST':
         file=request.files['image']
         filename=file.filename
         if filename=="":
-            flash("Please Insert a Images", "error")
+            flash(flash_msz, "error")
             return render_template('index.html')
         file_path = os.path.join(uploadedimges, filename)
         file.save(file_path)
         pred, output_page=brain_models_prediction(brain_Images=file_path)
         return render_template(output_page, pred_output=pred, user_image=file_path)
-
+#---------------------------------------------------------------------------------------------------------
+@app.route("/predict-lungs", methods=['POST'])
+def predict_lungs():
+    if request.method=='POST':
+        file=request.files['image']
+        filename=file.filename
+        if filename=="":
+            flash(flash_msz, "error")
+            return render_template('index.html')
+        file_path = os.path.join(uploadedimges, filename)
+        file.save(file_path)
+        pred, output_page=lungs_models_prediction(lungs_Images=file_path)
+        return render_template(output_page, pred_output=pred, user_image=file_path)
+#-------------------------------------------------------------------------------------------------------
+@app.route("/predict-skin", methods=['POST'])
+def predict_skin():
+    if request.method=='POST':
+        file=request.files['image']
+        filename=file.filename
+        if filename=="":
+            flash(flash_msz, "error")
+            return render_template('index.html')
+        file_path = os.path.join(uploadedimges, filename)
+        file.save(file_path)
+        pred, output_page=skin_models_prediction(skin_Images=file_path)
+        return render_template(output_page, pred_output=pred, user_image=file_path)
+#-----------------------------------------------------------------------------------------------------------
 
 if __name__=="__main__":
+    index='index.html'
+    credit='credit.html'
+    help='help.html'
+    flash_msz="Please Enter the Images. Image is not Inserted!!"
     app.run(debug=True)
